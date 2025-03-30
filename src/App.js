992676e3363e1329d834cda06496c0f724dccbdf
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-03-29 19:40:11 -0600

    problemas scripts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>¡Hola, React!</h1> {/* Cambié el texto aquí */}
+      <h1 className="text-6xl font-bold">¡Hola, React!</h1> 
     </div>
   );
 }
