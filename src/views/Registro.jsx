da7ad7facf8bd4420aba293bbdf7c904695f28ea
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-03-29 23:23:22 -0600

    Forms registro y logeo

--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -1,9 +1,98 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom';
+
 export default function Registro() {
   return (
-    <div>
-      Registros
+   
+    <>
+
+    <h1 className='text-4xl font-black '>
+      Crea tu cuenta
+    </h1>
+    <p>Crea tu cuenta llenando el formulario</p>
+
+    <div className='bg-white shadow rounded-lg p-10 mt-10'>
+
+    <form action="
+    ">
+
+<div className="mb-4">
+                        <label
+                            className="text-slate-800"
+                            htmlFor="name"
+                        >Nombre:</label>
+                        <input 
+                            type="text" 
+                            id="name"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            name="name"
+                            placeholder="Tu Nombre"
+                           
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label
+                            className="text-slate-800"
+                            htmlFor="email"
+                        >Email:</label>
+                        <input 
+                            type="email" 
+                            id="email"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            name="email"
+                            placeholder="Tu Email"
+                       
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label
+                            className="text-slate-800"
+                            htmlFor="password"
+                        >Password:</label>
+                        <input 
+                            type="password" 
+                            id="password"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            name="password"
+                            placeholder="Tu Password"
+                          
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label
+                            className="text-slate-800"
+                            htmlFor="password_confirmation"
+                        >Repetir Password:</label>
+                        <input 
+                            type="password" 
+                            id="password_confirmation"
+                            className="mt-2 w-full p-3 bg-gray-50"
+                            name="password_confirmation"
+                            placeholder="Repetir Password"
+                    
+                        />
+                    </div>
+
+                    <input
+                        type="submit"
+                        value="Crear Cuenta"
+                        className="bg-yellow-600 hover:bg-yellow-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+                    />
+              
+
+    </form>
     </div>
+
+    <nav className="mt-5">
+                 <Link to="/auth/login">
+                     ¿Ya tienes cuenta? Inicia Sesión
+                 </Link>
+             </nav>
+
+    </>
   )
 }
