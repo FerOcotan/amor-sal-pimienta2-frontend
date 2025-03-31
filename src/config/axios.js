54f1969de79ba69820dd98a3094306043e8ec9d1
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-03-30 22:59:42 -0600

    Add sactum

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const clienteAxios = axios.create({
     baseURL: process.env.REACT_APP_API_URL, // Cambiado para CRA
+    headers: {
+       'X-Requested-With': 'XMLHttpRequest',
+        "Accept": "application/json",
+    },
+    withCredentials: true, // Permitir cookies de terceros
+
 });
 
 export default clienteAxios;
